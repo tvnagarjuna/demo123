ff62c81f1f50cbf23d09f10833c0e5e29ac0bc88
unknown <tvnagarjuna@gmail.com>
2026-03-19 19:47:44 +0530

    Initial commit

---
+++ src/main/java/com/example/demo123/Demo123Application.java
@@ -0,0 +1,22 @@
+package com.example.demo123;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Demo123Application {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo123Application.class, args);
+    }
+    
+    @GetMapping("/")
+    public String home() {
+        return "Java CI/CD is Working 🚀";
+    }
+}
